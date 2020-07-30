4f6119a1167e2b2bd1ef52bd7a317614e228d14c
Juan Jose Mora <juanmora.dev@gmail.com>
2020-07-30 18:18:24 +0200

    Exo Tags ended with seed data

--- db/migrate/20200730151058_create_tags.rb
+++ db/migrate/20200730151058_create_tags.rb
@@ -2,7 +2,7 @@ class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
       t.string :title
-
+      
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-10.times do |x|
-  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Quotes::Shakespeare.hamlet_quote, email: Faker::Internet.email, age: Faker::Number.between(from: 18, to: 80))
-end
+# 10.times do |x|
+#   user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Quotes::Shakespeare.hamlet_quote, email: Faker::Internet.email, age: Faker::Number.between(from: 18, to: 80))
+# end
 
 
-10.times do |x|
-  city = City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code, user_id: Faker::Number.between(from: 1, to: 10))
-end
+# 10.times do |x|
+#   city = City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code, user_id: Faker::Number.between(from: 1, to: 10))
+# end
 
-20.times do |x|
-  gossip = Gossip.create(title: Faker::Lorem.paragraph_by_chars(number: 35, supplemental: false),content: Faker::Lorem.paragraph_by_chars(number: 80, supplemental: false), user_id: Faker::Number.between(from: 1, to: 10))
+# 20.times do |x|
+#   gossip = Gossip.create(title: Faker::Lorem.paragraph_by_chars(number: 35, supplemental: false),content: Faker::Lorem.paragraph_by_chars(number: 80, supplemental: false), user_id: Faker::Number.between(from: 1, to: 10))
+# end
+
+40.times do |x|
+  join = JoinTableGossipTag.create(gossip_id: Faker::Number.between(from: 1, to: 20), tag_id: Faker::Number.between(from: 1, to: 10))
 end
